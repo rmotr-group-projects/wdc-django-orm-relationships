2a4a8ad733b0e8eb2835662ffdc28eb27731c042
Santiago Basulto <santiago.basulto@gmail.com>
2018-12-06 11:48:07 -0300

    Improve tests

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ### Setup Instruction
 
-The structure of the whole Django project is built for you. Run the following commands in order to have your local environment up and running.  
+The structure of the whole Django project is built for you. Run the following commands in order to have your local environment up and running.
 
 ```bash
 $ mkvirtualenv -p $(which python3) django_orm_relationships
@@ -30,5 +30,7 @@ Each of the task has its instructions as a docstring, so use them as a guide to 
 In order to check if you implemented them correctly, there are tests associated to each task inside the `artists/tests.py`. You can run the tests like this:
 
 ```bash
-$ make test
+$ py.test django_orm_relationships/artists/tests.py -k task_1
+$ py.test django_orm_relationships/artists/tests.py -k task_2
+$ py.test django_orm_relationships/artists/tests.py -k task_N  # replace with the current function
 ```
--- django_orm_relationships/artists/orm_exercises.py
+++ django_orm_relationships/artists/orm_exercises.py
@@ -9,13 +9,13 @@ def task_1_artist_exists():
 
 def task_2_first_song_ordered():
     """Should return the first Song ordered by title"""
-    # HINT: Use .first() function
+    # HINT: Use .first() function
     pass
 
 
 def task_3_last_artist_ordered():
     """Should return the last Artist ordered by artistic_name"""
-    # HINT: Use .last() function
+    # HINT: Use .last() function
     pass
 
 
@@ -27,13 +27,13 @@ def task_4_artist_songs_contains():
 
 def task_5_songs_exclude():
     """Should return all songs excluding the ones that contains the word 'Thrill' in its title"""
-    # HINT: Use .exclude() function
+    # HINT: Use .exclude() function
     pass
 
 
 def task_6_artist_name_starts_with():
     """Should return the amount of artists whose artistic name starts with the pattern 'Ji'"""
-    # HINT: Use __startswith field lookup and .count() function
+    # HINT: Use __startswith field lookup and .count() function
     pass
 
 
@@ -48,7 +48,7 @@ def task_7_get_or_create_artist():
 
 def task_8_artist_songs_reverse_relationship():
     """Should return all songs from artist Stevie Wonders using reverse relationships"""
-    # step 1: get Artist "Stevie Wonders"
+    # step 1: get Artist "Stevie Wonders"
     artist = '...'
 
     # step 2: return all songs from that artist using .song_set reverse relationship
@@ -57,11 +57,11 @@ def task_8_artist_songs_reverse_relationship():
 
 def task_9_update_song_artist():
     """Should create a new Artist and assign it as the owner of the song called Superstition"""
-    # step 1: create the artist
+    # step 1: create the artist
     artist = '...'
 
-    # step 2: get the song called 'Superstition'
+    # step 2: get the song called 'Superstition'
     song = '...'
 
-    # step 3: assign created artist to the song and save() the song model
+    # step 3: assign created artist to the song and save() the song model
     pass
